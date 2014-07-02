94205423719dc1996f808c20dce8c48f5399cda5
Eugene <bignoga@ukr.net>
2014-07-02 19:37:43 +0300

    First classes added

---
+++ src/main/java/ua/com/hata/EstateObject.java
@@ -0,0 +1,8 @@
+package ua.com.hata;
+
+/**
+ * Created by Eugene on 02.07.2014.
+ */
+public interface EstateObject {
+
+}
---
+++ src/main/java/ua/com/hata/SiteUser.java
@@ -0,0 +1,7 @@
+package ua.com.hata;
+
+/**
+ * Created by Eugene on 02.07.2014.
+ */
+public class SiteUser implements User {
+}
